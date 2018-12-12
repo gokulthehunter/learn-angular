5ce773f0ef659efe96e39e87d56beefa12d8753a
gokul <gokul@22feettribalww.com>
2018-12-12 23:32:24 +0530

    first chapter done

--- first-app/src/app/app.module.ts
+++ first-app/src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { WarningAlertComponent } from './warning/warning.component';
+import { SuccessComponent } from './success/success.component';
+import { DangerComponent } from './danger/danger.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent
+    ServerComponent,
+    ServersComponent,
+    WarningAlertComponent,
+    SuccessComponent,
+    DangerComponent
   ],
   imports: [
     BrowserModule
